bca634edbc4929272554491437f89ff67dcf3516
Javad Cheraghi <javad.music1371@gmail.com>
2022-12-13 18:18:23 +0330

    Updates

---
+++ precache-manifest.0121efe5f9cbcb7f3d2b25da76692b9f.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "051fbfe2939677b952cdac1757e04cf6",
+    "url": "/Main_YouShop_React/index.html"
+  },
+  {
+    "revision": "f1c5ec2cb39717f46fff",
+    "url": "/Main_YouShop_React/static/css/2.9109016a.chunk.css"
+  },
+  {
+    "revision": "e231185134e94b2b02c6",
+    "url": "/Main_YouShop_React/static/css/main.2aea833c.chunk.css"
+  },
+  {
+    "revision": "f1c5ec2cb39717f46fff",
+    "url": "/Main_YouShop_React/static/js/2.8bef163b.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/Main_YouShop_React/static/js/2.8bef163b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e231185134e94b2b02c6",
+    "url": "/Main_YouShop_React/static/js/main.8a00ec24.chunk.js"
+  },
+  {
+    "revision": "82e5c3c31b324415e422",
+    "url": "/Main_YouShop_React/static/js/runtime-main.b7eb2ed3.js"
+  },
+  {
+    "revision": "28ee6fa698bb58e67d73670186cdc4e4",
+    "url": "/Main_YouShop_React/static/media/24-hours-support.28ee6fa6.svg"
+  },
+  {
+    "revision": "823b29bcaf01fd4d5f05609698541a99",
+    "url": "/Main_YouShop_React/static/media/404.823b29bc.svg"
+  },
+  {
+    "revision": "4b570687fbe6346b087d14ab741d4d9e",
+    "url": "/Main_YouShop_React/static/media/Apple.4b570687.png"
+  },
+  {
+    "revision": "64228fd6dfc8ad29549be2abc8bf62dd",
+    "url": "/Main_YouShop_React/static/media/Canon.64228fd6.png"
+  },
+  {
+    "revision": "202844344d6a62a63cf090b80638f065",
+    "url": "/Main_YouShop_React/static/media/Huawi.20284434.png"
+  },
+  {
+    "revision": "6d8df3f002a0fca8ee46dce367f22595",
+    "url": "/Main_YouShop_React/static/media/LG.6d8df3f0.png"
+  },
+  {
+    "revision": "8bb1c115c111f82b967c9a65ea891e09",
+    "url": "/Main_YouShop_React/static/media/Samsung.8bb1c115.png"
+  },
+  {
+    "revision": "a91231a66c2be0dfc22723c9347cde98",
+    "url": "/Main_YouShop_React/static/media/Vazir.a91231a6.woff2"
+  },
+  {
+    "revision": "abdaaf2e791f4416323efffec0a85b49",
+    "url": "/Main_YouShop_React/static/media/Vazir.abdaaf2e.woff"
+  },
+  {
+    "revision": "9b08a31884b39b3753f7a2fd528a6c5a",
+    "url": "/Main_YouShop_React/static/media/Xiaomi - Mi 9T.9b08a318.jpg"
+  },
+  {
+    "revision": "0bd9f6505afe7327ee590b508adb1552",
+    "url": "/Main_YouShop_React/static/media/add.0bd9f650.svg"
+  },
+  {
+    "revision": "8ee737be6433aed9bddf68fd87a24b5c",
+    "url": "/Main_YouShop_React/static/media/arrow.8ee737be.svg"
+  },
+  {
+    "revision": "6adc922ab9028ba3602af07444038250",
+    "url": "/Main_YouShop_React/static/media/curve1.6adc922a.svg"
+  },
+  {
+    "revision": "964b50f1ed7294df52c75291ffe33887",
+    "url": "/Main_YouShop_React/static/media/curve2.964b50f1.svg"
+  },
+  {
+    "revision": "b5942a247cc2f1f48a4697f3fc08e56f",
+    "url": "/Main_YouShop_React/static/media/detailSvg.b5942a24.svg"
+  },
+  {
+    "revision": "183cc64f8241b0a45d15771bc906a90a",
+    "url": "/Main_YouShop_React/static/media/emptyCart.183cc64f.svg"
+  },
+  {
+    "revision": "c21dae0e93b530306fef444e9e5938da",
+    "url": "/Main_YouShop_React/static/media/fashion-banner_1.c21dae0e.jpg"
+  },
+  {
+    "revision": "c6ccfd71fa08e9a51852ba772601c90e",
+    "url": "/Main_YouShop_React/static/media/fastSend.c6ccfd71.svg"
+  },
+  {
+    "revision": "12e3d04e13b7e76d0a1fe3134d7a2f9e",
+    "url": "/Main_YouShop_React/static/media/logo.12e3d04e.svg"
+  },
+  {
+    "revision": "e4344a0c87f1285baa68da660c8d705c",
+    "url": "/Main_YouShop_React/static/media/navPng.e4344a0c.png"
+  },
+  {
+    "revision": "3f5e99b366ad1247ead57c18ee05eaf6",
+    "url": "/Main_YouShop_React/static/media/original.3f5e99b3.svg"
+  },
+  {
+    "revision": "54f417139a4297eef1a748718a44d6ec",
+    "url": "/Main_YouShop_React/static/media/pulseLoading.54f41713.svg"
+  },
+  {
+    "revision": "82edcf1a449cc39ae9980e89ff32ad45",
+    "url": "/Main_YouShop_React/static/media/return.82edcf1a.svg"
+  },
+  {
+    "revision": "e7357ee44db1b58ea459f4f13e2eda0c",
+    "url": "/Main_YouShop_React/static/media/search.e7357ee4.svg"
+  },
+  {
+    "revision": "851c3b556641038c819112fe1375598a",
+    "url": "/Main_YouShop_React/static/media/svg-shape2.851c3b55.svg"
+  },
+  {
+    "revision": "3181d135de5dc1f621677ece668c660f",
+    "url": "/Main_YouShop_React/static/media/tshirt_1.3181d135.jpg"
+  },
+  {
+    "revision": "f725dcc569c2046beafa903691d217a9",
+    "url": "/Main_YouShop_React/static/media/web_heart_animation.f725dcc5.png"
+  },
+  {
+    "revision": "e083717531e888292a8f6878532ea938",
+    "url": "/Main_YouShop_React/static/media/xiaomi mi 10_1.e0837175.jpg"
+  }
+]);
